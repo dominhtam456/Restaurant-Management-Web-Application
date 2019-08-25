735514b87d94e0e4dd5cc44276e9253c026b72bc
Khang Nguyen <khangnguyen2356@gmail.com>
2019-08-25 16:50:01 +0700

    Add new food-detail.html

--- argon-dashboard-master/assets/js/masterpage.js
+++ argon-dashboard-master/assets/js/masterpage.js
@@ -20,6 +20,10 @@ app.config(function($routeProvider) {
     templateUrl : "/examples/order.html",
     controller : "TablesCtrl"
   })
+  .when("/food-details", {
+    templateUrl : "/examples/food-details.html",
+    controller : ""//nho them controller
+  })
   .when("/login", {
     templateUrl : "/examples/login.html",
     controller : "Login.IndexController",
